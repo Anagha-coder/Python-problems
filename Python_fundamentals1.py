05d00fd58d0ab48e06d839e0a3497fcfb8add1dc
Anagha-coder <71918177+Anagha-coder@users.noreply.github.com>
2021-01-09 07:28:03 +0530

    Update and rename Python fundamentals1 to Python_fundamentals1.py

--- Python_fundamentals1.py
+++ Python_fundamentals1.py
@@ -1,6 +1,10 @@
 """ PYTHON FUNDAMENTALS:PRACTICE QUESTIONS """
-"""Given: a boolean value
-Return: a 'Yes' string for true and a 'No' string for false"""
+"""
+
+Given: a boolean value
+Return: a 'Yes' string for true and a 'No' string for false
+
+"""
 
 def bool_to_word(boolean):
     if boolean == True:
